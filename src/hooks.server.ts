444f81a50b7560dbe0c0fa7d20bad1bb8451ac7a
agoodman1999 <agoodman@civiconnect.ca>
2023-11-29 08:19:03 -0500

    server hook for updating score

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,47 @@
 // src/hooks.server.ts
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+import {supabase} from '$lib/server/supabaseServerClient';
 import type { Handle } from '@sveltejs/kit';
+import type { Score } from '$types/general';
+import { isScore } from '$types/general';
+
+async function submitScore(event: any) {
+	const supabaseAuthCookie = event.cookies.get('sb-bbbrnwinzilcycqgvzed-auth-token');
+
+	// just get the first string in the stringified array
+	if (!supabaseAuthCookie) {
+		console.log('no auth cookie')
+		return
+	}
+
+	const jwt = supabaseAuthCookie.split('"')[1];
+
+	// check who's making the request
+	const { data: userRes } = await supabase.auth.getUser(jwt);
+	const user = userRes.user;
+
+	if (!user) {
+		console.log('no user')
+		return 
+	}
+
+	//get score from request body
+	const score: Score = {part_1: 10, part_2: 10, placement: 10, total: 10}
+
+	console.log(user.id)
+
+	//insert score into db
+	const res = await supabase.from('Score').insert([{
+		user_id: user.id,
+		...score
+	}])
+
+	if (res.error) {
+		console.log(res.error)
+		return
+	}
+}
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.supabase = createSupabaseServerClient({
@@ -21,6 +61,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 		} = await event.locals.supabase.auth.getSession();
 		return session;
 	};
+
+	//on submit-solution
+	if (event.url.pathname.startsWith('/api/submit-solution')) {
+		await submitScore(event)
+	}
 
 	return resolve(event, {
 		filterSerializedResponseHeaders(name) {
